d9a76adce4eb7af4a4e0db7a571545575f9b6b9d
Cledson <cledson1996@gmail.com>
2022-12-20 20:10:44 -0300

    ufaa

--- src/controllers/deletapedidos.controller.js
+++ src/controllers/deletapedidos.controller.js
@@ -1,22 +1,30 @@
 import { pedidos } from "../database/db.js";
+import { ObjectId } from "mongodb";
 
 
 export async function deletapedidos(req, res) {
-    let pedido = req.headers.pedido;
-    
-    console.log(pedido)
-    try {
-        let mensage = await pedidos.findOne({ pedido });
-      
-        if (!mensage) {
-            res.status(404).send(req.body)
-            return
+    let pedido = req.body.pedido
+
+
+    let vaii = pedido.map((ref)=>{return {_id: new ObjectId(ref.id)}})
+    console.log(vaii)
+  
+    for (let i = 0; i < pedido.length; i++) {
+        console.log(vaii[i])
+               
+        let achou = await pedidos.deleteOne(vaii[i]);
+        console.log(achou)
+            
+        try {
+           
+            
         }
-        await pedidos.deleteOne({ pedido });
-        res.send("pedido apagada com sucesso!");
-    } catch (err) {
-        console.log(err);
-        res.sendStatus(404);
+        catch (err) {
+            console.log(err);
+            res.sendStatus(404);
+        }
     }
+
+
 
 }
