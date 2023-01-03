bb1a81be37b35e892e0695374385969f9db6f043
Cledson <cledson1996@gmail.com>
2023-01-02 21:59:19 -0300

    dormir

--- src/controllers/pedidos.controller.js
+++ src/controllers/pedidos.controller.js
@@ -1,13 +1,13 @@
 import { pedidos } from "../database/db.js";
 
 export async function pedidoss(req, res) {
-    
+
     let registros
-   
+
     if (req.headers.chave == "pedido" && req.headers.filtro) {
-       console.log("entrei")
-       console.log(req.headers.filtro)
-        registros = await pedidos.find({ pedido: {'$regex' : `${req.headers.filtro}`, '$options' : 'i'}}).toArray();
+        console.log("entrei")
+        console.log(req.headers.filtro)
+        registros = await pedidos.find({ pedido: { '$regex': `${req.headers.filtro}`, '$options': 'i' } }).toArray();
         try {
             res.status(200).send(registros);
         } catch (err) {
@@ -16,25 +16,40 @@ export async function pedidoss(req, res) {
 
 
     } else if (req.headers.chave == "motoboy" && req.headers.filtro) {
-        registros = await pedidos.find({ motoboy: {'$regex' : `${req.headers.filtro}`, '$options' : 'i'}}).toArray();
+        registros = await pedidos.find({ motoboy: { '$regex': `${req.headers.filtro}`, '$options': 'i' } }).toArray();
         try {
             res.status(200).send(registros);
         } catch (err) {
             res.status(500).send(err);
         }
-    } else if (req.headers.chave == "data" && req.headers.filtro){
-        registros = await pedidos.find({ data: {'$regex' : `${req.headers.filtro}`, '$options' : 'i'}}).toArray();
+    } else if (req.headers.chave == "data" && req.headers.filtro) {
+        console.log(req.headers.filtro)
+        registros = await pedidos.find({ data: { '$regex': `${req.headers.filtro}`, '$options': 'i' } }).toArray();
         try {
             res.status(200).send(registros);
         } catch (err) {
             res.status(500).send(err);
         }
     }
-    else{
-      
+    else if (req.headers.chave == "dataa") {
+        console.log(req.headers.filtro)
+        console.log(req.headers.filtro2)
+        let antigo = new Date(req.headers.filtro).getTime()
+        let novo = new Date(req.headers.filtro2).getTime()
+        registros = await pedidos.find({ data: { $gt: antigo, $lt: novo } }).toArray();
+
+        try {
+
+            res.status(200).send(registros);
+        } catch (err) {
+            res.status(500).send(err);
+        }
+    }
+    else {
+
         registros = await pedidos.find({}).toArray();
         try {
-          
+
             res.status(200).send(registros);
         } catch (err) {
             res.status(500).send(err);
--- src/models/cadastro_pedidos.models.js
+++ src/models/cadastro_pedidos.models.js
@@ -4,15 +4,14 @@ export const cadastro_pedidosSchema = joi.object({
     pedido: joi.string().required().min(2).max(50),
     motoboy: joi.string().required(),
     login: joi.string().required().min(3),
-    data: joi.string().min(3).required(),
+    data: joi.date().min(3).required(),
     cliente: joi.string().required(),
-    name:joi.string(),
-    img:joi.string(),
-    qtd:joi.any(),
-    horas: joi.string(),
-    ausente:joi.number(),
-  
+    name: joi.string(),
+    img: joi.string(),
+    qtd: joi.any(),
+    ausente: joi.number(),
 
-   
+
+
 });
 
